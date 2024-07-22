2fdaf869e2dcd78bac162e2eededed9e6417015f
S. Sergey <SS86RG@ya.ru>
2024-07-22 15:13:21 +0300

    second commit DZ

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree tree = mainTree();
+        System.out.println(tree);
+
+
+
+    }
+    static FamilyTree mainTree(){
+        FamilyTree tree = new FamilyTree();
+        int i=0;
+        Human vasya = new Human(i++, "Василий", Gender.male,  null, null, LocalDate.of(1980,6,24), null);
+
+        Human masha = new Human( i++, "Маша", Gender.female, null, null, LocalDate.of(1985, 6, 15), null);
+
+
+        tree.add(vasya);
+        tree.add(masha);
+
+//
+        tree.setWedding(vasya, masha);
+//
+        Human lena = new Human(i++, "Лена", Gender.female, vasya, masha, LocalDate.of(2010,7,23), null);
+
+        Human ivan = new Human(i++, "Иван", Gender.male,vasya, masha, LocalDate.of(2005, 7, 1), null);
+
+        tree.add (lena);
+        tree.add (ivan);
+
+        Human Inna = new Human(i++, "Инна", Gender.female,null, null, LocalDate.of(1954,11,25), null);
+        Inna.addChild(vasya);
+        tree.add(Inna);
+//
+
+        return tree;
+    }
+}
