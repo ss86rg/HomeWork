6eb942b1d3ae0fbbe85bec30b47723ae1c77900a
S. Sergey <SS86RG@ya.ru>
2024-07-30 22:51:16 +0300

    second commit DZ4

---
+++ src/Animals.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface Animals <E>extends Serializable {
+    int getId();
+    void setId(int id);
+    void add (E human);
+    String getName();
+    int getAge();
+    LocalDate getBirthDate();
+    LocalDate getDeathDate();
+    List<E> getChildren();
+    E getMarried();
+    void setMarried(E human);
+
+    boolean addParent(E parent);
+    boolean addChild(E child);
+    boolean setWedding (E human1, E human2);
+    List<E> getParents();
+}
